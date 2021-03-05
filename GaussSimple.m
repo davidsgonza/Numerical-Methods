df2ea2bade725c80c7aa073641e88fc48e4a605f
davidgonzas <davgonza44@gmail.com>
2021-03-04 21:15:45 -0500

    Final test

--- GaussSimple.m
+++ GaussSimple.m
@@ -1,4 +1,4 @@
-%Funcion que resuelve sistema de ecuaciones por el metodo de gauss
+% Function that solves system of equations by the Gaussian method
 %Input
 %A= data matrix
 %b= right side vector
@@ -7,11 +7,11 @@
 
 
 function x=GaussSimple(A,b) 
-[m,n]=size(A);
-if m~=n , error ('La matriz debe ser cudrada'); end
-nb=n+1;
-Aug=[A,b];
-
+    [m,n]=size(A);
+    if m~=n , error ('The matrix must be square'); end
+    nb=n+1;
+    Aug=[A,b];
+    
 %Elimination
 
 for k=1:n-1
@@ -20,12 +20,11 @@ for k=1:n-1
         Aug(i,k:nb)=Aug(i,k:nb)-factor*Aug(k,k:nb);
     end
 end
-%Sustitution
-x=zeros(n,1); 
-x(n)=Aug(n,nb)/Aug(n,n);
-for i=n-1:-1:1
-    x(i)=(Aug(i,nb)-Aug(i,i+1:n)*x(i+1:n))/Aug(i,i);
-end
-
+    %Sustitution
+    x=zeros(n,1); 
+    x(n)=Aug(n,nb)/Aug(n,n);
+    for i=n-1:-1:1
+        x(i)=(Aug(i,nb)-Aug(i,i+1:n)*x(i+1:n))/Aug(i,i);
+    end
 
 end
